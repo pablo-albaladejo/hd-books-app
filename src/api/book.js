888ce016e2530db50bfd6d6b29af64da31ffdf9f
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-05-24 15:18:34 +0200

    feat(): leasing api request

--- src/api/book.js
+++ src/api/book.js
@@ -2,11 +2,22 @@ import apiFetch from './apiFetch'
 import Config from '../../config.json'
 
 export const getBook = (data = {}) =>
-   apiFetch({
+  apiFetch({
     baseUrl: data.baseUrl,
     token: data.token,
     endPoint: encodeURI(Config.API.ENDPOINT.BOOK + '/' + data.id),
-    method: Config.API.ENDPOINT.GET,
+    method: Config.API.METHOD.GET,
     body: data.body,
     headers: data.headers
   });
+
+export const postBook = (data = {}) =>
+  apiFetch({
+    baseUrl: data.baseUrl,
+    token: data.token,
+    endPoint: encodeURI(Config.API.ENDPOINT.BOOK),
+    method: Config.API.METHOD.POST,
+    body: data.body,
+    headers: data.headers
+  });
+
--- src/services/book.js
+++ src/services/book.js
@@ -1,12 +1,33 @@
-import { getBook } from '../api/book'
+import { getBook, postBook } from '../api/book'
 
 export const getAllBooks = () => {
     return getBook({
         id: 'list'
     })
 }
-export const getBookById = () => {
+export const getBookById = (id) => {
     return getBook({
-        id: 'list'
+        id,
+    })
+}
+
+export const leaseBook = (user_id, book_id) => {
+    return postBook({
+        body: {
+            user_id,
+            book_id,
+            status: 1
+        }
+    })
+}
+
+
+export const unleaseBook = (user_id, book_id) => {
+    return postBook({
+        body: {
+            user_id,
+            book_id,
+            status: 1
+        }
     })
 }
