28518fba6b862cdeacced52b81cee97ce3c31f02
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2019-05-24 13:27:34 +0200

    feat(): fetch book by id

--- src/screens/book.js
+++ src/screens/book.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-
+import { Text, View } from 'react-native';
+import *  as BookService from '../services/book'
 export default class Book extends Component {
 
+    state = {
+        book: null
+    }
+
+    componentDidMount() {
+
+        BookService.getBookById(this.props.navigation.state.params.id)
+            .then(book => {
+                this.setState({
+                    book
+                })
+            })
+
+    }
+
     render() {
+        const { book } = this.state;
         return (
-            <Text>{'BOOK:' + this.props.navigation.state.params.id}</Text>
+            <View>
+                {book && (
+                    <Text>
+                        {'BOOK:' + JSON.stringify(book)}
+                    </Text>
+                )}
+            </View>
         )
     }
 }
